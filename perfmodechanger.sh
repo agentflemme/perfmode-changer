cd5d9b1db2788978635e66ea22079ccf73ec83ec
agentflemme <48907859+agentflemme@users.noreply.github.com>
2023-11-22 23:22:14 +0100

    Update perfmodechanger.sh

--- perfmodechanger.sh
+++ perfmodechanger.sh
@@ -31,42 +31,60 @@ if [[ $1 == "" ]] ; then
 echo "checking power profile"
 profiler
 
-if [ $profile == bugged ] 
-then
+if [[ $profile == "bugged" ]] ; then
 echo "Power profile bugged, setting profile to battery"
 echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig
 fi
 
-if [ $profile == battery ] 
-then
+if [[ $profile == "battery" ]] ; then
 echo "seems like your device is in battery mode, changing to performance mode"
 echo 10 > /sys/devices/virtual/thermal/thermal_message/sconfig
 fi
 
-if [ ${powah} == performance ] 
-then
+if [[ ${powah} == performance ]] ; then
 echo "seems like your device is in performance mode, changing to battery mode"
 echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig
 fi
+
+
 profiler
 echo "power profile successfully changed to $profile !"
 exit 1
 fi
 
 #Interactive mode, if you want to switch manually to a specific mode.
+
 if [[ $1 == "-i" ]] ; then
 profiler
 echo -e "Current power profile is $profile.\nAvailable power profiles :\n - Battery 0\n Nolimits (Performance+NoThermals) 1\n - MGame (Performance) 2"
 read choice
-if [[ $choice == "0" ]] ; then ; echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
-if [[ $choice == "1" ]] ; then ; echo 10 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
-if [[ $choice == "2" ]] ; then ; echo 20 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
-if [[ $choice -ne "0" ]] || [[ $choice -ne "1" ]] || [[ $choice -ne "2" ]] ; then ; echo "wrong choice try again" ; fi
+
+if [[ $choice == "0" ]] ; then ; 
+echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
+if [[ $choice == "1" ]] ; then
+echo 10 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
+if [[ $choice == "2" ]] ; then
+echo 20 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
+
+if [[ $choice -ne "0" ]] || [[ $choice -ne "1" ]] || [[ $choice -ne "2" ]] ; then
+echo "wrong choice try again"
+exit 1
+fi
+
 profiler ; echo "Power profile changed to $profile"
 exit 1
 fi
 
 #Arguments Mode, to switch plans fast
-if [[ $1 == "0" ]] ; then ; echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
-if [[ $1 == "10" ]] ; then ; echo 10 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
-if [[ $1 == "20" ]] ; then ; echo 20 > /sys/devices/virtual/thermal/thermal_message/sconfig ; fi
+if [[ $1 == "0" ]] ; then
+echo 0 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
+if [[ $1 == "10" ]] ; then
+echo 10 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
+if [[ $1 == "20" ]] ; then
+echo 20 > /sys/devices/virtual/thermal/thermal_message/sconfig
+fi
